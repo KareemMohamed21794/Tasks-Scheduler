e381c1ffc19297870aa1049f84aa2383ecdc8876
Hesham Ghoniem <hesham.a.ghoniem@gmail.com>
2023-02-13 16:29:02 +0200

    Suggestions

--- app/Console/Commands/scheduler_command.php
+++ app/Console/Commands/scheduler_command.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 use App\Models\Task;
 use App\Models\TaskHistory;
+use Exception;
 use Illuminate\Console\Command;
 use Mail;
 use App\Mail\TaskMail;
@@ -26,49 +27,36 @@ class scheduler_command extends Command
     /**
      * Execute the console command.
      *
-     * @return int
+     * @return void
      */
-    public function handle()
+    public function handle(): void
     {
         $taskId = $this->argument('task_id');
         $task = Task::find($taskId);
         $data['task'] = $task;
         $response = Http::get($task->task_url);
         $data['status'] = $response->status();
-        
+
         try {
 
-            if($response->status() == '200'){
-                /////////Add Task History/////////////
-                $objTaskHistory = new TaskHistory();
-                $objTaskHistory->task_id = $taskId;
-                $objTaskHistory->response = $response->status();
-                $objTaskHistory->body = $response->body();
-                $objTaskHistory->date = date('Y-m-d H:i:s');
-                $objTaskHistory->save();
-                print_r('Success Status');die;
-            }else{
-              
+            if($response->status() != '200'){
                 Mail::send('email.test', $data, function ($message)use($data,$task) {
                     $message->subject('Task Mail');
                     $message->to('testmailk17@gmail.com');
                 });
-
-                /////////Add Task History/////////////
-                $objTaskHistory = new TaskHistory();
-                $objTaskHistory->task_id = $taskId;
-                $objTaskHistory->response = $response->status();
-                $objTaskHistory->body = $response->body();
-                $objTaskHistory->date = date('Y-m-d H:i:s');
-                $objTaskHistory->save();
-                print_r('Error Status');die;
             }
-            
-            
+
+            TaskHistory::create([
+                'task_id' => $taskId,
+                'response' => $response->status(),
+                'body' => $response->body(),
+                'date' => date('Y-m-d H:i:s')
+            ]);
+
         } catch (Exception $e) {
 
             print_r('Sorry Something Error');die;
-              
+
         }
     }
 
